3a8af27ffd251e0f0ae9ecc93ff63eb1623c5517
Nikita <tulenckov.nikita@gmail.com>
2023-09-10 20:37:54 +0300

    Усовершенсововал тесты в поместив их в класс

--- tests/get_tests.py
+++ tests/get_tests.py
@@ -1,33 +1,35 @@
+import pytest
 from Base.Basic_API_methods import BaseMetods
 
 
-def test_get_user(base_url):
-    """
-    Проверяет, что статус-код ответа равен 200 (успешный запрос).
-    Проверяет, что данные о пользователе в ответе соответствуют ожидаемым значениям,
-    а именно, что id равно 1 и username равно 'Bret'.
-    """
-    url = base_url
-    response = BaseMetods().send_get_request(url, "users/1")
-    assert response["id"] == 1
-    assert response["username"] == 'Bret'
+class TestsGet:
 
+    @pytest.fixture
+    def setup(self, base_url):
+        self.base_url = base_url
 
-def test_get_posts(base_url):
-    """
-    Проверяет, что статус-код ответа равен 200 (успешный запрос).
-    Проверяет, что в ответе есть посты, путем проверки, что длина списка постов больше нуля.
-    """
-    url = base_url
-    response = BaseMetods.send_get_request(url, "posts?userId=1")
-    assert len(response) > 0
+    def test_get_user(self, setup):
+        """
+        Проверяет, что статус-код ответа равен 200 (успешный запрос).
+        Проверяет, что данные о пользователе в ответе соответствуют ожидаемым значениям,
+        а именно, что id равно 1 и username равно 'Bret'.
+        """
+        response = BaseMetods().send_get_request(self.base_url, "users/1")
+        assert response["id"] == 1
+        assert response["username"] == 'Bret'
 
+    def test_get_posts(self, setup):
+        """
+        Проверяет, что статус-код ответа равен 200 (успешный запрос).
+        Проверяет, что в ответе есть посты, путем проверки, что длина списка постов больше нуля.
+        """
+        response = BaseMetods.send_get_request(self.base_url, "posts?userId=1")
+        assert len(response) > 0
 
-def test_get_comments(base_url):
-    """
-    Проверяет, что статус-код ответа равен 200 (успешный запрос).
-    Проверяет, что в ответе есть комментарии, путем проверки, что длина списка комментариев больше нуля.
-    """
-    url = base_url
-    response = BaseMetods.send_get_request(url, "comments?postId=1")
-    assert len(response) > 0
+    def test_get_comments(self, setup):
+        """
+        Проверяет, что статус-код ответа равен 200 (успешный запрос).
+        Проверяет, что в ответе есть комментарии, путем проверки, что длина списка комментариев больше нуля.
+        """
+        response = BaseMetods.send_get_request(self.base_url, "comments?postId=1")
+        assert len(response) > 0
